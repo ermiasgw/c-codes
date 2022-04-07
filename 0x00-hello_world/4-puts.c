f214f74a001a6782cd4738af28bc316a57d0a58b
ermias <ermiasgashaw7881@gmail.com>
2022-04-07 08:26:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /*main method */
 int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
