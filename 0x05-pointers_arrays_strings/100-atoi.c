acda06f49279163b2108d58b0a0b614cc1159ea2
ermias <ermiasgashaw7881@gmail.com>
2022-04-18 13:45:52 -0700

    edited now

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - convert
+ * @s: string
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int n = 1;
+
+	while (s[i] < '0' && s[i] > '9' && s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			n = n * -1;
+		}
+		i++;
+	}
+	if (s[i] == '\0')
+	{
+		n = 0;
+		return (n);
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		n = n * 10 - (s[i] - '0');
+		i++;
+	}
+	return (n);
+}
