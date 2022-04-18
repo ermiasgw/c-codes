8bc613b29f50b7520c602f91937adc08b27d5907
ermias <ermiasgashaw7881@gmail.com>
2022-04-18 11:58:37 -0700

    edited now

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,16 @@
 
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	while (*(s+i) != '\0')
+	while (*(s + i) != '\0')
 	{
 		i++;
 	}
 	i--;
 	while (i >= 0)
 	{
-		_putchar(*(s+i));
+		_putchar(*(s + i));
 		i--;
 	}
 	_putchar('\n');
