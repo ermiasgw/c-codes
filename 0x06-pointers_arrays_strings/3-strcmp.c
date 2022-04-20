3d950bfd94df8a7541ca97e598ee4b0ac9477038
ermias <ermiasgashaw7881@gmail.com>
2022-04-20 13:54:26 -0700

    ok

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,18 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int count = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0')
 	{
-		count = count + (s1[i] - s2[i]);
+		if (s1[i] > s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else if (s1[i] < s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
 	}
-	return (count);
+	return (0);
 }
