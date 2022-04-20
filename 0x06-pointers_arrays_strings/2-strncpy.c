def7810216158af6d104d24d10dfd1e055b407b1
ermias <ermiasgashaw7881@gmail.com>
2022-04-20 13:05:55 -0700

    ok

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string.
+ * @dest: string
+ * @src: string
+ * @n: integer
+ * Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (j < n)
+	{
+		if (src[j] == '\0')
+		{
+			*dest = *src;
+			break;
+		}
+		else
+		{
+			dest[j] = src[j];
+			j++;
+		}
+	}
+	return (dest);
+}
+
