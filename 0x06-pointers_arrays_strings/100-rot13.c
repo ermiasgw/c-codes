55d478ca9770b103cb8db2e373dba228bc54a67a
ermias <ermiasgashaw7881@gmail.com>
2022-04-21 14:25:41 -0700

    8th question

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,10 +9,22 @@
 char *rot13(char *a)
 {
 	int i = 0;
+	int j = 0;
+	char *p = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *o = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	while (a[i] != '\0')
 	{
-		a[i] = a[i] + 13;
+		while (p[j] != '\0')
+		{
+			if (a[i] == p[j])
+			{
+				a[i] = o[j];
+				break;
+			}
+			j++;
+		}
+		j = 0;
 		i++;
 	}
 	return (a);
