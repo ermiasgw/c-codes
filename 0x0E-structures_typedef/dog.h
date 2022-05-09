08197060c01db39fffe28d791918e3294b1b59d8
ermias <ermiasgashaw7881@gmail.com>
2022-05-09 00:07:56 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+/**
+ * struct dog - struture of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+} dog_t;
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
